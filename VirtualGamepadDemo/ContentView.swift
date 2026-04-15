2608abce3ab26d66ef026c54b8ff3ded874aab35
jonah.fu <ferock@gmail.com>
2026-04-15 14:42:19 +0800

    Initial Commit

---
+++ VirtualGamepadDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VirtualGamepadDemo
+//
+//  Created by 傅璎俊 on 2026/4/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VirtualGamepadDemo/VirtualGamepadDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VirtualGamepadDemoApp.swift
+//  VirtualGamepadDemo
+//
+//  Created by 傅璎俊 on 2026/4/15.
+//
+
+import SwiftUI
+
+@main
+struct VirtualGamepadDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
